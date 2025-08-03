9ba9ca1335944b683b07c5137c351a750d35c2a2
Rishabh Tiwari <rt8932484@gmail.com>
2025-08-03 12:40:05 +0000

    datatype  js

---
+++ d01_datatype.js
@@ -0,0 +1,17 @@
+//IN THIS CODE I LEARN DATA TYPE VAR , LET , CONST
+const accountid = 2.8
+let accountemail = "rt8932484@gmail,com"
+var accountpass = "12345"
+
+accountcity = "deoria"
+let accountstate;
+// accountid = 2
+accountemail = "rt@gmail.com"
+accountpass = "123222"
+accountcity = "benglore"
+//const accountid = 3;
+
+accountstate
+
+console.log(accountid);
+console.table([accountid,accountemail,accountpass,accountcity,accountstate]);
---
+++ d01p1_datatype.js
@@ -0,0 +1,27 @@
+// in this code i learn strict and num variable stored null and undefined datatpe
+"use strict";//treat all js  code as newer version
+
+//alert(3 + 3 );WE ARE USING NODE JS ,NOT BROWSER
+console.log(3 + 3 );
+console.log("RISHBH");//CODE READABILITY SHOULD BE HIGH
+let name = "Rishabh";
+let age = 25;  
+let isloggedIn = true;
+let state = null;
+
+//primitive datatype*/
+
+//number =>2 to power 53
+//bigint is use in big site like trading 
+//string is also use in single '' and double "" also
+//boolean is => true /false
+// null is stadloan value type
+//udefined =>value not assign
+//synbol=>unique 
+// null is object
+
+
+//object
+console.log(typeof null);
+console.log(typeof undefined);
+
