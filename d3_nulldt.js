12bf275501da039cc12e99a2dfe62e28dba61254
Rishabh Tiwari <rt8932484@gmail.com>
2025-08-03 14:22:58 +0000

    string advance js

---
+++ d3_nulldt.js
@@ -0,0 +1,14 @@
+// in this code i learn null operation and === 
+// console.log("2" > 1);op true 
+// console.log("02" > 1);op true 
+
+
+// console.log(null == 0);//false op
+// console.log(null < 0);//op false
+// console.log(null < 0);// op false
+// console.log(null >= 0);//op true 
+
+// console.log(undefined == 0);//op false 
+//=== it strictly check and datatype also check in this 
+
+// console.log("2" === 2); // false 
---
+++ d4_strings.js
@@ -0,0 +1,36 @@
+const name = "Rishabh_Tiwari"
+const repo = 50
+//console.log(name + repo + " value ");
+//console.log(`My Name is ${name} and my repo count is ${repo}`);//` it is standlon
+
+
+const gameName = new String('Rishabh-hc-com')
+console.log(gameName[0]);
+// console.log(gameName.__proto__);//prototype tells
+
+
+// console.log(gameName.length);//length known of string
+// console.log(gameName.toUpperCase());//upercase 
+// console.log(gameName.charAt(2));//char position by no.
+console.log(gameName.indexOf('i'));//charater position by char 
+
+//create new string 
+const newString = gameName.substring(1, 5)
+console.log(newString);//substring do no. range to output their character 
+
+//another string
+
+const anotherString = gameName.slice(-5,4)// it can take negativ value from there char 
+
+console.log(anotherString);
+ 
+const newStringOne = "    Rishabh    "
+console.log(newStringOne);
+console.log(newStringOne.trim());//trim do space remove in string by using this 
+
+const url = "https://rishabh.com/rishabh%20tiwari"
+
+console.log(url.replace('%20' , '-'))
+console.log(url.includes(' Rishabh '));
+console.log();
+console.log(gameName.split('_'));
