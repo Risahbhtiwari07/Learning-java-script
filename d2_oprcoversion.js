580162df98032e7ee644b1e1a79633306c4fa7c0
Rishabh Tiwari <rt8932484@gmail.com>
2025-08-03 12:47:52 +0000

    conversion js

---
+++ d2_oprcoversion.js
@@ -0,0 +1,63 @@
+//in this code i learn to add str and operation arithmetic and basic function syntax
+/////////*******OPERATION******** *///////////
+
+// const { userOne } = require("./userOne");
+
+
+// let value = "3"
+// let negvalue = -value
+// console.log(negvalue);
+
+
+// console.log(2+2);
+
+// console.log(2-2);
+
+// console.log(2*2);
+
+// console.log(2**2);
+
+// console.log(2/2);
+
+// console.log(2%2);
+
+// let str1 = "hello"
+
+// let str2 ="Rishabh"
+
+// let str3 = str1 + str2 // strig can add also 
+// console.log(str3);
+
+
+// console.log(1 + " 2");
+// console.log(2 + " 1");
+// console.log(2 + 3 + "2");
+
+// console.log(+true);
+// console.log(+ "");
+
+
+// let num1,num2,num3 = 2 + 2
+// num1 = num2 = num3 = 2 + 2
+
+
+// let gamecounter = 100
+// gamecounter++;
+// console.log(gamecounter);
+//------steak and heap 
+let anothername = "Rani";
+anothername = "mani";
+
+console.log(anothername); // Output: mani
+
+let userOne = {
+    email: "user@yahoo.com",
+    upi: "abc@ybl"
+};
+
+let userTwo = userOne; // Assigning by reference (not a copy)
+
+userTwo.email = "rishabh@yahoo.com"; // This changes both userTwo and userOne's email
+
+console.log(userOne.email);  // Output: rishabh@yahoo.com
+console.log(userTwo.email);  // Output: rishabh@yahoo.com
---
+++ d2_typeconversion.js
@@ -0,0 +1,32 @@
+// in this code i learn type consion and typeof data store 
+let stringnumber = 56
+let stringnumbery = String (stringnumber)
+console.log(stringnumbery);
+console.log(typeof stringnumber);
+
+// console.log(typeof (score));
+// console.log(typeof score);
+
+
+// let valueinumber = Number (score)
+// console.log(typeof valueinumber);
+// console.log(valueinumber);
+
+
+// if we coverting any ''33''. then it easily a no. converts 33
+//"33abc convert then it not convert o/p is NaN"
+//true is convert in 1 or falsre in 0 
+
+
+
+// let isloggedIn = "rsiahbh"
+// let booleanisloggedIn = Boolean(isloggedIn)
+// console.log(booleanisloggedIn);
+
+
+//if we write 1 bolean then it show true values or 0 is false display
+//if we do empty ""in this boolen then false
+// if we write "rishabh " then it display true 
+
+
+
