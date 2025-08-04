65ad31d2e13dadc220733441931c4683118b325d
Rishabh Tiwari <rt8932484@gmail.com>
2025-08-04 03:39:45 +0000

    numbers and maths js

---
+++ d5_num_and_maths.js
@@ -0,0 +1,47 @@
+// in this we are learnin about number and maths about js 
+
+
+const score = 500
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+ 
+
+const  othernumber = 123.3422
+// console.log(othernumber.toPrecision(4));
+
+
+const hundred = 1000000
+// console.log(hundred.toLocaleString('en-in'));
+
+
+
+// maths learning in js 
+
+
+// console.log(Math);
+// console.log(Math.abs(-4));//absolute value is use to do negative to positive
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2)); //ceil is use to round up the number
+// console.log(Math.floor(4.9));
+// console.log(Math.max(4, 5, 6, 7, 8, 9));
+// console.log(Math.min(4, 5, 6, 7, 8, 9));
+
+console.log(Math.random());// random number between 0 and 1
+console.log(Math.random()*10 + 1); // random number between 1 and 10
+
+console.log((Math.random()*10) + 1);
+
+const min = 10 
+const max = 20
+
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+// random number between 10 and 20
+
+
