26d73a0524229abb27b2e1f67965a34fc6f8d52e
Rishabh Tiwari <rt8932484@gmail.com>
2025-08-27 07:22:11 +0000

    array

--- d7_array.js
+++ d7_array.js
@@ -8,26 +8,11 @@ const myarr2 = new Array(1, 2, 3, 4)
 // console.log(myarr[1]);
 
 
-//array methods
 
-// myarr.push(6)//push methodd adds the array element
-// myarr.push(8)
-// myarr.pop()
-// myarr.unshift(9)//in this add in startpoint in array
-// myarr.shift()//remove the start point 
-// console.log(myarr.includes(8));  i t check the que include or not 
-// console.log(myarr.indexOf(5));//index count
-// const newArr = myarr.join()
-// console.log(typeof newArr);
-// console.log(myarr);
-
-
-
-//slice, splice
 
 console.log("A", myarr);
 
-const myn1 = myarr.slice(1, 3)
+const myn1 = myarr.pop(1, 3)
 console.log(myn1);
 
 console.log("B", myarr);
@@ -36,3 +21,32 @@ const myn2 = myarr.splice(1, 3)
 console.log(myn2);
 console.log("C", myarr);
 console.log();
+
+// array
+
+// push():      its used to insert element in the last on the array
+// pop():       its used to remove last element in the array.
+// unshift():   its used to insert elemnet in the starting on the array.
+// shift():     its used to remove first element in the array
+
+// includes():  its used to check passed argument are have array or not.
+// indexOf():   its used to know the indexOf of passed argument.
+// join():      its used to get parent array element and convert it intothe string.
+// slice():     its used to return part of array slice(startFrom, endTo) and endTo not include.
+// splice():    its used to slice array, its include start and or point and remove that part in the array.
+// push():      when we push an array in push() method then, its push array as an element.
+// concat():    its used to make new array with caller and passed array elements.
+// spread:      its used to spraed all element of array.
+// let car = ['Alto', 'Baleno', 'Creta', 'Ertiga', 'Indigo'];
+// let supercar = ['Supra', 'Prsche', 'Maclaren'];
+// let mxicar = [...car, ...supercar];
+// console.log(mxicar);
+
+// flat():      its used to return element of all array and sub-array.
+// let myArr = [1, 2, 3, [4, 5], 6, [7, 8, [9, [10, 11], 12], 13], 15];
+// myArr = myArr.flat(Infinity);
+// console.log(myArr);
+
+// isArray():   its used to check passed argument is array or not and its return boolean value.
+// from():      its used to make passed argument in array;
+// of():        its return new array of passed element.
