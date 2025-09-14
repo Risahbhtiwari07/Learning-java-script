de567e97a806ffa9eb8d4871afc427c9c6dc67ad
Rishabh Tiwari <rt8932484@gmail.com>
2025-09-14 11:12:55 +0000

    array part 2

---
+++ array2.js
@@ -0,0 +1,28 @@
+const marvel_heros = ["thor", "ironman", "spiderman"]
+const dc_heros = ["superman", "flash", "batman"]
+
+// marvel_heros.push(dc_heros)
+
+// console.log(marvel_heros );
+
+// console.log(marvel_heros [3][1]);
+
+// const all_heros = marvel_heros.concat(dc_heros) //concat [ 'thor', 'ironman', 'spiderman', 'superman', 'flash', 'batman' ]
+// console.log(all_heros);
+
+
+const all_new_heros = [...marvel_heros, ...dc_heros]
+console.log(all_new_heros);
+
+
+
+console.log(Array.isArray("Rishabh"));
+console.log(Array.from("Rishabh"));
+console.log(Array.from({name: "Rishabh"}))
+ 
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3));
