d55a1f732d529539886fbc28cf4298df9d7b01ba
Rishabh Tiwari <rt8932484@gmail.com>
2025-09-17 08:57:56 +0000

    IIFE

---
+++ iife.js
@@ -0,0 +1,14 @@
+//IMMEDIATELY INVOKED FUNCTION EXPRESSIONS (IIFE)
+
+(function chai(){
+    //named iife
+    console.log(`db connected`);
+    
+})();
+
+
+
+//unnamed iife
+( (name) => {
+    console.log(`db connected two ${name} `);
+} )("rishabh") 
