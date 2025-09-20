4825a46182931be7c909f1ed56753d83f8fb79f8
Rishabh Tiwari <rt8932484@gmail.com>
2025-09-20 12:49:52 +0000

    switch case

---
+++ flow control/two.js
@@ -0,0 +1,57 @@
+// in this swith case
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+// in this case match by no.
+// const month = 1
+// switch (month) {
+//     case 1:
+//     console.log("january")
+//         break;
+// case 2:
+//     console.log("feb")
+//         break;
+// case 3:
+//     console.log("march")
+//       //  break;
+// case 4:
+//     console.log("april")
+//         break;
+// case 5:
+//     console.log("may")
+//         break;
+
+//     default:
+//         console.log("default case match ");
+//         break;
+// }
+
+
+// in this case match by string
+const month = "march"
+switch (month) {
+    case "jan":
+    console.log("january")
+        break;
+case "feb":
+    console.log("feb")
+        break;
+case "march":
+    console.log("march")
+     break;
+case "april":
+    console.log("april")
+        break;
+case "may":
+    console.log("may")
+        break;
+
+    default:
+        console.log("default case match ");
+        break;
+}
