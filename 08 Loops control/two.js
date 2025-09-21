56fd4b9cd2673c25bdcad3b9ff6c6197b85c0553
Rishabh Tiwari <your-email@example.com>
2025-09-21 12:15:26 +0000

    while loop and do while loop

---
+++ 08 Loops control/two.js
@@ -0,0 +1,23 @@
+// while 
+let index = 0
+while (index <= 25) {
+    //console.log(`value of index is ${index}`);
+    index = index + 2
+        
+}
+
+let myArray = ['flash', 'Superman','batman']
+let arr = 0
+while (arr< myArray.length) {
+    //console.log(`value id ${myArray[arr]}`);
+    arr = arr + 1
+}
+
+//do while
+
+let score = 1
+
+do {
+    console.log(`score  is ${score}`);
+    
+} while (score<=10);
